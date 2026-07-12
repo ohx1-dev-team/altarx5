9ee680b75de92ff569251244401b08e397d54805
Parsley Ohxi <km7bnv@proton.me>
2026-07-11 17:06:55 -0700

    test change

--- public/client.js
+++ public/client.js
@@ -157,10 +157,10 @@ function addMessageToScreen(msg) {
   if (msg.time) {
     const date = new Date(msg.time);
     // Format: "HH:MM AM/PM"
-    timeString = date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+    timeString = date.toLocaleTimeString([], { hour: '2-dight', minute: '2-digit' });
   } else {
     const now = new Date();
-    timeString = now.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+    timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   }
   
   timeDiv.textContent = timeString;
